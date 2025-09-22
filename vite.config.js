00d0f13e0942b151719b4376a291c46e5cb60240
brunomendez <mendezbruno1995@gmail.com>
2025-09-22 14:40:18 -0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+
+  // --- AÑADE ESTA SECCIÓN ---
+  assetsInclude: ['**/*.glb'],
+  // ---------------------------
+
   resolve: {
     alias: {
-      // Esto te permite usar '@/' para referirte a la carpeta 'src'
-      '@': path.resolve(process.cwd(), 'src'),
-    },
-  },
-  // Esta línea es CRUCIAL para que la importación del .glb funcione
-  assetsInclude: ['**/*.glb'],
+      '~bootstrap': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'node_modules/bootstrap'),
+    }
+  }
 })
