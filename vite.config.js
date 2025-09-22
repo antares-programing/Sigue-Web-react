bee2c426b21e9dc45097162cc8600853ccf3f208
brunomendez <mendezbruno1995@gmail.com>
2025-09-22 14:43:31 -0300

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import path from 'path'
+
+// No necesitas 'path' ni 'fileURLToPath' si no usas alias
 
 export default defineConfig({
   plugins: [react()],
 
-  // --- AÑADE ESTA SECCIÓN ---
-  assetsInclude: ['**/*.glb'],
-  // ---------------------------
-
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'node_modules/bootstrap'),
-    }
-  }
+  // Esto es correcto y debe quedarse
+  assetsInclude: ['**/*.glb'], 
 })
